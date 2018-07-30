564d25c0ff881a18e16d479427dac81ff0964d3f
Petr Čaněk <canekpet@gmail.com>
2018-07-30 11:10:18 +0200

    Added packages and test data

---
+++ src/testData.js
@@ -0,0 +1,81 @@
+const bookList = [
+  {
+    id: 'book-1',
+    name: 'Malý princ',
+    description: `Není princ jako princ. Tenhle je skutečně jen jeden – Malý princ Antoina de Saint-Exuperyho, jedno z nejslavnějších děl moderní světové literatury.
+
+Malý princ, kouzelná pohádková bytost, přichází na naši Zemi ze vzdálených vesmírných světů, aby se kdesi v Africké poušti setkal s autorem našeho příběhu a zjevil mu tajemství své podivuhodné životní pouti. Ale zjeví mu vlastně daleko víc: tajemství čistého srdce, dobra a krásy. Je to opravdu líbezná knížka, která vám bude jistě právě tak milá a blízká, jako byla všem čtenářským generacím, a k níž se budete i později najednou rádi vracet.`,
+    author: 'author-1',
+  },
+  {
+    id: 'book-2',
+    name: 'Tanec s draky',
+    description: `
+Na východě, sužována svými nepřáteli, vládne jako královna města postaveného na prachu a smrti Daenerys, poslední potomek rodu Targaryenů, jejíž draci vyrostli do hrůzné dospělosti. Nyní, když je známo místo jejího výskytu, se mnozí vydali Daenerys a její draky hledat. Mezi nimi i trpaslík, Tyrion Lannister. Mezi další hledače patří dornský princ, železný kapitán i tajemný modrovlasý mladík, který zdaleka není tím, za koho jej vydávají... Na severu stojí velká Zeď z ledu a kamene – stavba pevná jenom tak, jako jsou pevní ti, kdo na ní slouží. Jejím 998. velitelem byl zvolen nemanželský syn Eddarda Starka, Jon Sníh, který má nepřátele jak v Hlídce, tak za Zdí, kde se šikují k útoku armády divokých... A za Zdí je i malý chlapec, jeho bratr Bran, obdařený dosud netušenými schopnostmi.`,
+    author: 'author-2',
+  },
+  {
+    id: 'book-3',
+    name: 'Krátká vítězná válka',
+    description: `Třetí kniha série Honor Harringtonová.
+
+Vláda Havenu zabředá do vážných vnitropolitických potíží a domnívá se, že je opět dokáže na čas zatlačit do pozadí úspěchem v nové válce. Už padesát let jim tato politika vycházela. V cestě jim však stojí Mantichorské království a jeho spojenci ležící v oblasti mezi oběma vesmírnými říšemi.
+
+Kapitán Harringtonová i celé mantichorské loďstvo jsou připraveni se postarat o to, aby válka zdaleka nebyla pro Haven snadná, natožpak vítězná...`,
+    author: 'author-3',
+  },
+  {
+    id: 'book-4',
+    name: 'Za každou cenu',
+    description: `
+Válka s republikou Haven znovu začala… pro Hvězdné království Mantichory katastrofálně.
+
+Amirál lady dame Honor Harringtonová, místodržící a vévodkyně z Harringtonu, jediná vítězná spojenecká velitelka počáteční fáze nové války, byla povolána ze soustavy Sidemore, aby velela Osmé flotile. Každý ví, že Osmá flotila je základní obranná jednotka Aliance, což z ní dělá přirozené místo pro ženu, kterou média nazvala „Salamandrem“. Ale co většina veřejnosti neví, je fakt, že Hvězdné království a jeho spojenci nejenom že čelí značné přesile, ale že se dá čekat, že se to bude postupně ještě zhoršovat. Úkolem Osmé flotily je udělat něco, aby nepřítel nerozdrtil Alianci dřív, než Hvězdné království bude mít šanci dosáhnout strategické rovnováhy. Toho nelze dosáhnout lacino. Honor Harringtonová bude muset přijmout děsivou zodpovědnost za případné ztráty a k tomu se ještě vypořádat s převratnými změnami ve svém osobním a společenském životě.
+
+Alternativou ke splnění jejího úkolu je drtivá porážka, avšak v tomto případě může být i cena vítězství strašlivá.`,
+    author: 'author-3',
+  },
+];
+
+const authorList = [
+  {
+    id: 'author-1',
+    name: 'Antoine de Saint-Exupéry',
+    biography: `Celým jménem Antoine-Jean-Babtiste-Marie-Roger de Saint-Exupéry, narodil se v Lyonu a tragicky zahynul v létě 1944, patrně sestřelen německým letadlem v prostoru Korsiky nebo nad Alpami.
+
+Jeho život i tvorba jsou mimořádně sourodé, obojí určuje Saint-Exupéryho povolání letce, ale i psychické založení hluboce citlivého a ušlechtilého člověka. Po studiu na Námořní škole, které ho nijak nenadchlo, sloužil jako voják u letectva, kde propukla jeho vášeň pro létání. Jako pilot byl zaměstnán u vícero leteckých společností a létal na různých linkách. Exotická prostředí, v nichž se ocitají jeho letečtí hrdinové, důvěrně znal, neboť se pohyboval na linkách Toulouse-Dakar, Casablanka-Timbuktu, Buenos Aires-Ohňová země; pokusil se i o rekordní lety a několikrát havaroval. Po vypuknutí 2. světové války si doslova vynutil vstup do řad válečných pilotů, který mu byl pro jeho věk odpírán. Jeho dvacetiletou leteckou dráhu završila tragická smrt, která jeho život i dílo korunovala heroickou slávou.
+
+Saint-Exupéryho literární dílo je apoteózou mravních hodnot člověka. Třebaže skeptik, prožívající trvalou bolestnou úzkost ze zmechanizované, odlidštěné civilizace, neustále osvědčuje optimistickou víru v to nejlepší v člověka i v lidském společenství. Jeho dílo, pateticky vášnivé a myšlenkově hluboké, vzbudilo už od prvních knih živý čtenářský ohlas. Po novele Letec vydal romány Kurýr na jih, Noční let, Země lidí (s ústředním motivem ztroskotání nad pouští, které zažil na vlastní kůži) aj. Velmi známá je jeho básnická pohádka Malý princ. Některá díla (Citadela, Dopis rukojmímu, deníky, korespondence atd.) vyšla až po autorově smrti.`,
+  },
+  {
+    id: 'author-2',
+    name: 'George R. R. Martin',
+    biography: `George Raymond Richard Martin (* 20.9.1948 v Bayonne, New Jersey). Jeho otec byl přístavní dělník. Měl dvě sestry.
+
+Navštěvoval Mary Jane Donohoe School a střední školu Marist High School. Začal psát jako velmi mladý, prodával příběhy o příšerách ostatním dětem ze sousedství za penny, a to všetně dramatického čtení. Později se stal fanouškem komických knížek a sběratelem na střední škole a začal psát beletrii pro amatérské fanouškovské časopisy. Jeho první profesionální prodej se uskutečnil v roce 1970 , kdy mu bylo 21: “The Hero” (=hrdina) prodán Galaxy, vydán v únoru 1971. Další prodeje následovaly.
+
+V roce 1970 získal bakalářský titul v žurnalistice na Severozápadní univerzitě v Evanstonu, Illinois, (s červeným diplomem). Následně tamtéž dokončil magisterské studium žurnalistiky.
+
+Jakožto svědomitý oponent sloužil náhradní službu v letech 1972-1974 u VISTA při nadaci bezplatné právní pomoci okrsku Cook. Též dělal ředitele šachových turnajů pro Continental Chess Association (kontinentální šachová asociace) v letech 1973-1976, a v letech 1976-1978 vyučoval žurnalistiku na Clarke College, Dubuque, Iowa. V 70. letech psal na částečný úvazek zatímco pracoval jako dobrovolník u VISTA, šachový ředitel a učitel.
+
+V roce 1975 se oženil s Gale Burnick. Z bezdětného manželství se rozvedli se o čtyři roky později. Martin začal psát na plný úvazek v roce 1979. V letech 1978-79 působil jako writer-in-residence na Clarke College.
+
+Po přesunu do Hollywoodu se zapsal jako story editor pro Twilight Zone na CBS Television (1986). V následujícím roce se v CBS stává výkonným konzultantem pro Beauty and the Beast (Kráska a zvíře), za další rok jeho producentem a za další rok co-Supervising producentem. Byl výkonným producentem pro Doorways, pilotní projekt, který napsal pro Columbia Pictures Television, který byl zfilmován v letech 1992-93.
+
+Jeho současným domovem je Santa Fe v Novém Mexiku. Je členem Science Fiction & Fantasy Writers of America (američtí spisovatelé sci-fi a fantasy), kde působil v letech 1977-1979 jako regionální ředitel pro středo-jih a v letech 1996-1998 jako viceprezident. Je též členem Writers’ Guild of America, West (cech spisovatelů Ameriky, západ).`,
+  },
+  {
+    id: 'author-3',
+    name: 'David Weber',
+    biography: `Americký spisovatel sci-fi a fantasy. Narozen 24.10.1952. Vystudoval historii, politiku, anglickou literaturu a sociologii. Nyní žije v Greensville v Jižní Karolíně.
+
+Mnohá jeho díla mají silně militaristický náboj a odehrávají se v námořnickém prostředí. Je řazen mezi autory military science fiction. V jeho příbězích je zobrazován velmi komplexní a propracovaný svět – a to jak po stránce technologické tak sociální. Dokonce i když píše fantasy, je magie pojímána spíše jako jiná technologie s vlastními zákony a principy.
+
+Nejznámějším z jeho hrdinů je Honor Harringtonová. Kolem této hrdinky vystavěl autor celý vesmír známý pod názvem „Honorverse“. Ten zahrnuje víc než dvanáct románů a několik sbírek povídek. Vesmír má vlastní politiku, pokročilé technologie, náboženství, kulturu i faunu a flóru. Hlavní příběh se však odehrává kolem válečného konfliktu mezi Mantichorským královstvím a Lidovou republikou Haven. Před Honor staví autor nelehké úkoly, které tato překonává svou odvahou, taktikou a vůlí, s níž jako kapitánka křižníků motivuje svou posádku k nejlepším výkonům.`,
+  },
+];
+
+module.exports = {
+  bookList,
+  authorList,
+};
