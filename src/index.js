254c71194e4bd2de15969ff81c4367a80eb142e5
Petr Čaněk <canekpet@gmail.com>
2018-07-30 16:32:37 +0200

    initial info resolver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// TODO - build GraphQL server here :)
+const { GraphQLServer } = require('graphql-yoga');
+
+const resolvers = {
+  Query: {
+    info: () => {
+      return 'Test string';
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/typeDefs.graphql',
+  resolvers: resolvers,
+});
+
+server.start(() => console.log(`🚀  Server ready at http://localhost:4000`));
