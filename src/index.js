e90a160df62952984a64a630e6c22e05b3ad7405
Petr Čaněk <canekpet@gmail.com>
2018-09-20 19:12:14 +0200

    Added resolvers and types for static file data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// TODO - build GraphQL server here :)
+import { GraphQLServer } from 'graphql-yoga';
+
+import { authorList, bookList } from './testData';
+
+const resolvers = {
+  Query: {
+    info: () => 'This is info text',
+    getAuthorList: () => {
+      return authorList;
+    },
+    getBookList: () => {
+      return bookList;
+    },
+    getBook: () => {
+      return bookList[0];
+    },
+  },
+  Book: {
+    author: obj => {
+      return authorList.find(item => {
+        return item.id === obj.author;
+      });
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/typeDefs.graphql',
+  resolvers,
+});
+
+server.start(() => console.log('Server is running on localhost:4000'));
