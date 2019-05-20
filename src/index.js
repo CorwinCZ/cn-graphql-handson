6aad8d29b0ba07c00d4194755888eb82803e807b
Petr Čaněk <canekpet@gmail.com>
2019-05-20 17:57:08 +0200

    Added base of types and resolvers for the data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// TODO - build GraphQL server here :)
+import { GraphQLServer } from 'graphql-yoga';
+
+import { bookList, authorList } from './testData';
+
+const typeDefs = `
+  type Query {
+    infoQuery: String!
+    getBook: Book!
+    getAllBooks: [Book!]!
+    getAllAuthors: [Author!]!
+  }
+
+  type Book {
+    id: String!
+    name: String
+    description: String
+    author: String
+  }
+
+  type Author {
+    id: String!
+    name: String
+    biography: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    infoQuery: () => `Server is running`,
+    getBook: () => bookList[0],
+    getAllBooks: () => bookList,
+    getAllAuthors: () => authorList,
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log('Server is running on localhost:4000'));
