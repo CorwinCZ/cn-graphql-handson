745c2c5af75d4ce3800056d08c19bd9c6e891b0d
Petr Čaněk <canekpet@gmail.com>
2019-05-21 10:15:47 +0200

    added mutations

--- src/index.js
+++ src/index.js
@@ -10,29 +10,56 @@ const typeDefs = `
     getAllAuthors: [Author!]!
   }
 
+  type Mutation {
+    addBook(data: BookInput!): Book!
+  }
+
   type Book {
     id: String!
     name: String
     description: String
-    author: String
+    author: Author
   }
 
   type Author {
     id: String!
     name: String
     biography: String
+    allBooks: [Book!]!
+  }
+
+  input BookInput {
+    id: String!
+    name: String
+    description: String
+    author: String
   }
 `;
 
 const resolvers = {
   Query: {
     infoQuery: () => `Server is running`,
-    getBook: () => bookList[0],
+    getBook: () => bookList[1],
     getAllBooks: () => bookList,
     getAllAuthors: () => authorList,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      console.log('input', args.data);
+      return args.data;
+    },
+  },
+  Book: {
+    id: root => root.id,
+    author: root => authorList.find(item => item.id === root.author),
+  },
+  Author: {
+    allBooks: root => bookList.filter(item => item.author === root.id),
   },
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start(() => console.log('Server is running on localhost:4000'));
+server.start({ port: 4000 }, () =>
+  console.log('Server is running on localhost:4000'),
+);
